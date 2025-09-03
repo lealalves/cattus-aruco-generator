dd0ef8914ea1e608a153b8f470d5c0de59649ca1
Vinicius Leal Alves <leealalves@hotmail.com>
2025-09-02 22:11:08 -0300

    refactor: divide responsabilidades

---
+++ aruco_generator.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+class ArucoGenerator:
+    """Classe para gerar etiquetas ArUco personalizaveis."""
+
+    def __init__(self):
+        self.aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+
+    def generate_single_marker(self, marker_id: int, size: int = 200, margin: int = 10, border_bits: int = 1) -> np.ndarray:
+        marker_img = cv2.aruco.generateImageMarker(
+            self.aruco_dict,
+            marker_id,
+            size,
+            borderBits=border_bits
+        )
+        if margin > 0:
+            marker_img = cv2.copyMakeBorder(
+                marker_img, margin, margin, margin, margin, cv2.BORDER_CONSTANT, value=255
+            )
+        return marker_img
